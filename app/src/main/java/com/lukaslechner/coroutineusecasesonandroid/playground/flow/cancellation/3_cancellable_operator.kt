0139433fc77c2532861d0009d5b63863286993e9
jkotor <jkotor@gmail.com>
2024-03-20 13:26:31 +0100

    Flows cancellation

---
+++ app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/playground/flow/cancellation/3_cancellable_operator.kt
@@ -0,0 +1,39 @@
+package com.lukaslechner.coroutineusecasesonandroid.playground.flow.cancellation
+
+import kotlinx.coroutines.CancellationException
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.cancel
+import kotlinx.coroutines.flow.cancellable
+import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.flow.onCompletion
+import kotlinx.coroutines.launch
+import kotlin.coroutines.EmptyCoroutineContext
+
+suspend fun main() {
+    val scope = CoroutineScope(EmptyCoroutineContext)
+
+    scope.launch {
+        flowOf(1, 2, 3)
+            .onCompletion { throwable ->
+                if (throwable is CancellationException) {
+                    println("Flow got cancelled")
+                }
+            }
+            .cancellable()
+//            .onEach {
+//                println("Receive $it in onEach")
+//
+//                ensureActive()
+////                if (!currentCoroutineContext().job.isActive) {
+////                    throw CancellationException()
+////                }
+//            }
+            .collect {
+                println("Collected $it")
+
+                if (it == 2) {
+                    cancel()
+                }
+            }
+    }.join()
+}
