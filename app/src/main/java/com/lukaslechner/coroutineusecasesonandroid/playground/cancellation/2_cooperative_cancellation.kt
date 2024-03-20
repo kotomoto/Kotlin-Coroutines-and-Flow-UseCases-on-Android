4bd31962e7d5eb415096411f232f23928eb25edd
jkotor <jkotor@gmail.com>
2024-03-20 13:26:17 +0100

    Coroutine cooperative cancellation

--- app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/playground/cancellation/2_cooperative_cancellation.kt
+++ app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/playground/cancellation/2_cooperative_cancellation.kt
@@ -2,22 +2,28 @@ package com.lukaslechner.coroutineusecasesonandroid.playground.cancellation
 
 import kotlinx.coroutines.CancellationException
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.NonCancellable
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.isActive
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.withContext
 
 fun main() = runBlocking {
 
     val job = launch(Dispatchers.Default) {
         repeat(10) { index ->
-//            ensureActive() - 1
-//            yield() - 2
-            if (isActive) {
+//            ensureActive() // 1
+//            yield() // 2
+            if (isActive) { // 3
                 println("operation number $index")
                 Thread.sleep(100)
             } else {
 //                return@launch
+//                delay(100) // throws CancellableException
+                withContext(NonCancellable) {
+                    delay(100)
+                }
                 println("Cleanup ...")
                 throw CancellationException()
             }
